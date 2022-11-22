1112fdd704555591e91caf298042de31a489641c
RohitHattimare <rohithattimare1997@gmail.com>
2022-11-22 18:55:09 +0530

    Real Time Clock implimentation

--- time.js
+++ time.js
@@ -9,16 +9,18 @@ function currentTime() {
     if(hh > 12){
         session = "PM";
      }
-  
+
      hh = (hh < 10) ? "0" + hh : hh;
      mm = (mm < 10) ? "0" + mm : mm;
-     ss = (ss < 10) ? "0" + ss : ss;
-      
+     ss = (ss < 10) ? "0" + ss : ss;     
+     
+     //12 hour format
+     hh = (hh >= 12)? hh - 12 : hh;
+     
      let time = hh + ":" + mm + ":" + ss + " " + session;
-  
-    document.getElementById("clock").innerText = time; 
+     console.log(time);
+     document.getElementById("clock").innerText = time; 
     let t = setTimeout(function(){ currentTime() }, 1000); 
-  
   }
-  console.log(currentTime());
-//   currentTime();
+ console.log(currentTime());
+  // currentTime();
